c0449115f9e79e985ce89a8a5c5e07099fb2e83f
Kenyetta Griffin <kenyettagriffin@Kenyettas-MacBook-Air.local>
2022-01-26 22:28:07 -0500

    Reference Labb

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+  stores.push(store);
+  return stores;
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +15,24 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  stores.splice(index, 1);
+  return stores;
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  let secondStore = Object.assign({}, store);
+  //let secondStore = JSON.parse(JSON.sringify(store));
+   //store.a = 4;
+   //store.b.c = 4;
+  //let secondStore = stores.store;
+  return secondStore
+}
 
 module.exports = {
   addNewStore,
